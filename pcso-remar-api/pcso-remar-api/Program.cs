939fd250614ce20cc48821d83267cb54acdfc2bc
Remar Caballero <rcaballero@pcso.gov.ph>
2022-05-23 15:14:14 +0800

    Implement initial post

--- pcso-remar-api/pcso-remar-api/Program.cs
+++ pcso-remar-api/pcso-remar-api/Program.cs
@@ -7,6 +7,13 @@ var app = builder.Build();
     app.UseSwaggerUI();
     app.UseHttpsRedirection();
 
-    app.MapGet("Hello", () => "Hello from hot reload");
+    app.MapGet("Hello", () => "This is very cool I hope");
+    app.MapGet("Product", () => "Get some Product from Azure SQL");
+    app.MapPost("Product", (Product p) => "Prodcut saved in SQL");
+    app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
 
 app.Run();
+
+// Some Data Contracts
+public record class Message(string Name, string Msg);
+public record class Product(string Name, string Picture, int Price);
