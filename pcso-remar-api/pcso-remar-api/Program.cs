388c5cce83db8cc86a9cf42a41c31873c36c914d
Remar Caballero <rcaballero@pcso.gov.ph>
2022-05-23 15:19:37 +0800

    Implemented delete prodcut api

--- pcso-remar-api/pcso-remar-api/Program.cs
+++ pcso-remar-api/pcso-remar-api/Program.cs
@@ -3,14 +3,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection();
 
-    app.MapGet("Hello", () => "This is very cool I hope");
-    app.MapGet("Product", () => "Get some Product from Azure SQL");
-    app.MapPost("Product", (Product p) => "Prodcut saved in SQL");
-    app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+app.MapGet("Hello", () => "This is very cool I hope");
+app.MapGet("Product", () => "Get some Product from Azure SQL");
+
+app.MapPost("Product", (Product p) => "Product saved in SQL");
+app.MapDelete("Product", (string name) => "Deleted");
+
+
+app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
 
 app.Run();
 
